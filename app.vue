0ec0db80779bb04888690ebed14d60b47354ef6d
HirenBudhAir <hirenbudhrani@gmail.com>
2023-07-20 03:49:13 +0200

    adds new analysis system

--- app.vue
+++ app.vue
@@ -2,18 +2,32 @@
   <div class="flex flex-col">
     <form @submit.prevent="analyzeImages">
       <input type="file" ref="imageFiles" multiple required accept="image/*" />
-      <button type="submit" class="btn-primary mb-5 bg-black text-white">
+      <button type="submit" class="analyze-button" :disabled="processingPython">
         ANALYZE
       </button>
+      <div v-if="processingPython">
+        <div class="loading-spinner"></div> <!-- replace with your spinner element -->
+        <p>Processed {{processedFiles}} out of {{totalFiles}} files...</p>
+      </div>
+    </form>
+    <form @submit.prevent="analyzeImagesWithImagga">
+      <input type="file" ref="imageFilesForImagga" multiple required accept="image/*" />
+      <button type="submit" class="analyze-button" :disabled="processingImagga">
+        ANALYZE WITH IMAGGA
+      </button>
+      <div v-if="processingImagga">
+        <div class="loading-spinner"></div> <!-- replace with your spinner element -->
+        <p>Processed {{processedFiles}} out of {{totalFiles}} files...</p>
+      </div>
     </form>
     <div v-if="processedImages.length > 1" class="justify-end border-teal-950 border-y-pink-600">
       <div class="text-sm">Overall analysis</div>
-      <ProcessedImage :colors="totalImageData"/>
+      <ProcessedImage :colors="totalImageData" />
     </div>
     <br />
     <div class="flex flex-col">
       <div v-for="image in processedImages">
-        <ProcessedImage :sourceImage="image.sourceImage" :colors="image.colors" class="my-6"/>
+        <ProcessedImage :sourceImage="image.sourceImage" :colors="image.colors" class="my-6" />
       </div>
     </div>
   </div>
@@ -26,6 +40,9 @@ export default {
       imageUrlToAdd: "",
       colors: null,
       processedImages: [],
+      processingPython: false,
+      processingImagga: false,
+      processedFiles: 0,
       username: 'acc_0764885fd7bdbd6',
       password: '4cc177792332903bcc1292014b182cda',
     }
@@ -44,26 +61,29 @@ export default {
             percent: imageColor.percent / this.processedImages.length
           });
         });
-        image.colors.foreground_colors.forEach((imageColor) => {
+        image.colors.foreground_colors?.forEach((imageColor) => {
           totalColors.foreground_colors.push({
             html_code: imageColor.html_code,
             percent: imageColor.percent / this.processedImages.length
           });
         });
-        image.colors.background_colors.forEach((imageColor) => {
+        image.colors.background_colors?.forEach((imageColor) => {
           totalColors.background_colors.push({
             html_code: imageColor.html_code,
             percent: imageColor.percent / this.processedImages.length
           });
         });
       });
-      totalColors.image_colors.slice(0,10)
+      totalColors.image_colors.slice(0, 10)
       return totalColors;
     }
   },
   methods: {
-    async analyzeImages() {
-      const files = this.$refs.imageFiles.files;
+    async analyzeImagesWithImagga() {
+      const files = this.$refs.imageFilesForImagga.files;
+      this.totalFiles = files.length
+      this.processedFiles = 0
+      this.processingImagga = true
       for (let i = 0; i < files.length; i++) {
         try {
           const formData = new FormData();
@@ -91,11 +111,54 @@ export default {
           })
         } catch (error) {
           console.error(error);
+        } finally {
+          this.processedFiles += 1
+        }
+      }
+
+      this.$refs.imageFilesForImagga.value = null; // Reset the file input
+      this.processingImagga = false
+    },
+    async analyzeImages() {
+      const files = this.$refs.imageFiles.files;
+      this.totalFiles = files.length
+      this.processedFiles = 0
+      this.processingPython = true
+      for (let i = 0; i < files.length; i++) {
+        try {
+          const formData = new FormData();
+          formData.append('image', files[i]);
+
+          // Instead of Imagga API, use the local Python color analyzer running on localhost:8080
+          const response = await axios.post('https://goldfish-app-v7y4c.ondigitalocean.app/analyze', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          });
+
+          console.log(response);
+          const imageUrl = URL.createObjectURL(files[i]);
+          this.processedImages.push({
+            sourceImage: imageUrl,
+            colors: {
+              image_colors: response.data
+          }
+        })
+        } catch (error) {
+          console.info(error);
+        } finally {
+          this.processedFiles += 1
         }
       }
 
       this.$refs.imageFiles.value = null; // Reset the file input
+      this.processingPython = false
     }
   }
 }
 </script>
+<style>
+.analyze-button {
+  @apply px-5 py-3 shadow-sm transition ease-in-out duration-300 rounded leading-snug whitespace-nowrap text-base font-semibold
+}
+</style>
--- components/ProcessedImage.vue
+++ components/ProcessedImage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-row">
         <div v-if="sourceImage" class="pr-9">
-            <img :src="sourceImage" class="w-16 h-16" />
+            <img :src="sourceImage" class="w-36 h-auto" />
         </div>
         <div class="flex flex-row">
             <div v-if="colors.image_colors.length">
@@ -10,25 +10,26 @@
                 </div>
                 <div class="flex flex-col">
                     <div class="flex flex-row">
-                        <div class="pr-4">Color code</div>
+                        <div class="pr-4">Closest color</div>
                         <div>Percentage</div>
                     </div>
                     <div v-for="color in sortColors(colors.image_colors)" class="flex flex-row">
-                        <div :style="`background-color: ${color.html_code}`" class="mr-5 pr-3 pb-5">{{ color.html_code }}</div>
+                        <div :style="`background-color: ${color.html_code}`" class="mr-5 pr-3 pb-5">{{
+                            color.closest_palette_color }}</div>
                         <div>{{ color.percent }}</div>
                     </div>
                 </div>
             </div>
-            <div v-if="colors.foreground_colors.length">
+            <div v-if="colors.foreground_colors?.length">
                 <div class="text-lg italic pb-4">
                     Foreground colors
                 </div>
-                <div v-for="color in sortColors(colors.foreground_colors)" class="flex flex-row">
+                <div v-for="color in sortColors(colors.background_colors)" class="flex flex-row">
                     <div :style="`background-color: ${color.html_code}`" class="pr-5 pb-5">{{ color.html_code }}</div>
                     <div>{{ color.percent }}</div>
                 </div>
             </div>
-            <div v-if="colors.background_colors.length">
+            <div v-if="colors.background_colors?.length">
                 <div class="text-lg italic pb-4">
                     Background colors
                 </div>
@@ -48,7 +49,27 @@ export default {
     },
     methods: {
         sortColors(colors) {
-            return colors.sort((a,b) => b.percent - a.percent)
+            return colors.sort((a, b) => b.percent - a.percent)
+        }
+    },
+    computed: {
+        groupedColors() {
+            let grouped = {};
+            for (let color of this.colors.image_colors) {
+                let parent = color.closest_palette_color_parent;
+                if (parent === 'undefined') parent = 'Undefined Colors';
+
+                if (parent in grouped) {
+                    grouped[parent].colors.push(color);
+                    grouped[parent].totalPercentage += color.percent;
+                } else {
+                    grouped[parent] = {
+                        colors: [color],
+                        totalPercentage: color.percent
+                    };
+                }
+            }
+            return grouped;
         }
     }
 }
