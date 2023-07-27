3d488db1953791803bacdc6c14fb29a03cb9300d
HirenBudhAir <hirenbudhrani@gmail.com>
2023-07-27 03:06:42 +0200

    purge dem semicolons

--- components/ProcessedImage.vue
+++ components/ProcessedImage.vue
@@ -51,7 +51,7 @@ export default {
     },
     methods: {
         sortColors(colors) {
-            return colors.sort((a, b) => b.percent - a.percent);
+            return colors.sort((a, b) => b.percent - a.percent)
         },
     },
     computed: {
@@ -66,25 +66,25 @@ export default {
                     backgroundColor: [],
                     labels: [], // add a labels array to the child colors dataset
                 }
-            ];
-            let labels = [];
+            ]
+            let labels = []
 
             this.groupedColors.forEach(group => {
                 // Add each parent color to the outer layer
-                datasets[0].data.push(group.totalPercentage);
-                datasets[0].backgroundColor.push(group.hexColor);
-                labels.push(group.colorGroup);
+                datasets[0].data.push(group.totalPercentage)
+                datasets[0].backgroundColor.push(group.hexColor)
+                labels.push(group.colorGroup)
                 // Add each child color to the inner layer
                 group.colors.forEach(color => {
-                    datasets[1].data.push(color.percent);
-                    datasets[1].backgroundColor.push(color.html_code);
-                    datasets[1].labels.push(color.colorName); // add child color name to child dataset labels
-                });
-            });
+                    datasets[1].data.push(color.percent)
+                    datasets[1].backgroundColor.push(color.html_code)
+                    datasets[1].labels.push(color.colorName) // add child color name to child dataset labels
+                })
+            })
             return {
                 labels: labels,
                 datasets: datasets
-            };
+            }
         },
 
         // ...
@@ -93,13 +93,13 @@ export default {
             tooltips: {
                 callbacks: {
                     label: function (tooltipItem, data) {
-                        let dataset = data.datasets[tooltipItem.datasetIndex];
+                        let dataset = data.datasets[tooltipItem.datasetIndex]
                         if (dataset.labels && dataset.labels[tooltipItem.index]) {
                             // Use child color label if it exists
-                            return dataset.labels[tooltipItem.index];
+                            return dataset.labels[tooltipItem.index]
                         } else {
                             // Fall back to parent color label
-                            return data.labels[tooltipItem.index];
+                            return data.labels[tooltipItem.index]
                         }
                     }
                 }
@@ -107,18 +107,16 @@ export default {
             // ...
         },
         groupedColors() {
-            let colorGroups = [];
+            let colorGroups = []
             for (let color of this.colors.image_colors) {
-                console.log('grouped colors loop. color: ', color)
                 if (color.closest_palette_color_parent) {
                     let parent = color.closest_palette_color_parent
-                    console.log('CLOSEST PALETTE COLOR', parent)
                     if (parent === "undefined")
-                        parent = "Undefined Colors";
+                        parent = "Undefined Colors"
                     // Look for existing group
                     let group = colorGroups.find(g => g.colorGroup === parent)
                     if (group) {
-                        group.colors.push(color);
+                        group.colors.push(color)
                         group.totalPercentage += color.percent
                     }
                     else {
@@ -127,20 +125,15 @@ export default {
                             colors: [color],
                             totalPercentage: color.percent,
                             hexColor: color.closest_palette_color_parent_html_code || parent
-                        });
+                        })
                     }
                 }
             }
 
             // Sort color groups by totalPercentage
-            colorGroups.sort((a, b) => b.totalPercentage - a.totalPercentage);
-            return colorGroups;
+            colorGroups.sort((a, b) => b.totalPercentage - a.totalPercentage)
+            return colorGroups
         }
     },
-    async mounted() {
-        nextTick(() => {
-            console.info('hi bob')
-        }) // for chart to load
-    }
 }
 </script>
