f8d661955a2f33154a6166daec6860fddb661d5f
HirenBudhAir <hirenbudhrani@gmail.com>
2023-08-03 05:29:01 +0200

    Update info tooltip fix percent in overall analyis

--- app.vue
+++ app.vue
@@ -7,38 +7,23 @@
           ANALYZE LIKE YOU MEAN IT
         </button>
       </form>
+
+      <!-- Info tooltip -->
       <div id="info-tooltip" class="ml-7" @click.prevent="showInfo = !showInfo">
           <img v-if="!showInfo" class="w-9 cursor-pointer"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Minimalist_info_Icon.png/2048px-Minimalist_info_Icon.png" />
             <img v-if="showInfo" class="w-9 cursor-pointer"
             src="https://www.svgrepo.com/show/24584/info-icon.svg" />
         </div>
-        <div v-if="showInfo"
-          class="bg-white border border-gray-300 p-2 rounded-md shadow-md text-left mt-2 absolute top-20 left-1/2 transform -translate-x-1/2 z-20">
-          <!-- Your tooltip content here -->
-          <p class="mb-2 font-semibold">Version 1.0</p>
-          <p class="mb-2">How to use the Image Color Analyzer:</p>
-          <ol class="mb-2 list-disc list-inside">
-            <li>Choose one or more image files using the file input above.</li>
-            <li>Click the "Analyze" button to start the analysis.</li>
-          </ol>
-          <p class="mb-2 text-xl">What's coming in version 1.5:</p>
-          <ul class="mb-2 list-disc list-inside">
-            <li>Improved closest color and parent accuracy. <i>Use different color spaces such as CMYK, Lab, HSV to get the closest color.</i></li>
-            <li>Support for object analysis; when an image with a transparent background is provided.</li>
-          </ul>
-          <p class="mb-2 text-xl">What's coming in version 2:</p>
-          <ul class="list-disc list-inside">
-            <li>Save your current analysis to access later</li>
-            <li>Machine learning color analysis that will ideally get more accurate over time</li>
-            <li>Better visualizations: </li>
-            <li>User options: user can add analysis settings such as color filters, image compression,  </li>
-          </ul>
-        </div>
+        <InfoComponent v-if="showInfo" />
     </div>
 
     <div v-if="processingPython">
-      <span class="loader"></span> <!-- replace with your spinner element -->
+      <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
       <p>Processed {{ processedFiles }} out of {{ totalFiles }} files...</p>
     </div>
 
@@ -83,6 +68,7 @@ export default {
       this.processedImages.forEach((image) => {
         console.log('image from total images', image)
         image.colors.image_colors.forEach((imageColor) => {
+          imageColor.percent /= this.processedImages.length
           totalColors.image_colors.push(imageColor)
         })
       })
---
+++ components/InfoComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+        class="bg-white border border-gray-300 p-2 rounded-md shadow-md text-left mt-2 absolute top-20 left-1/2 transform -translate-x-1/2 z-20">
+        <p class="mb-2 font-semibold">Version 1.0</p>
+        <p class="mb-2">How to use the Image Color Analyzer:</p>
+        <ol class="mb-2 list-disc list-inside">
+            <li>Choose one or more image files using the file input above.</li>
+            <li>Click the "Analyze" button to start the analysis.</li>
+        </ol>
+        <p class="font-semibold">Pie Chart</p>
+        <ul>
+            <li>Outer circle = parent colors</li>
+            <li>Inner circle = actual colors</li>
+        </ul>
+        <br />
+        <div class="pt-10 border-l-fuchsia-700">
+            <p class="mb-2 text-xl">What's coming in version 1.5:</p>
+            <ul class="mb-2 list-disc list-inside">
+                <li>Improved closest color and parent accuracy. <i>Use different color spaces such as CMYK, Lab, HSV to get the
+                        closest color.</i></li>
+                <li>Support for object analysis; when an image with a transparent background is provided.</li>
+            </ul>
+            <p class="mb-2 text-xl">What's coming in version 2:</p>
+            <ul class="list-disc list-inside">
+                <li>Save your current analysis to access later</li>
+                <li>Machine learning color analysis that will ideally get more accurate over time</li>
+                <li>Better visualizations: </li>
+                <li>User options: user can add analysis settings such as color filters, image compression, </li>
+            </ul>
+        </div>
+    </div>
+</template>
