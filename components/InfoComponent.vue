fd89417a5f681e906f66a0826dfef772ee370c80
HirenBudhAir <hirenbudhrani@gmail.com>
2023-08-27 19:59:57 +0200

    updates info

--- components/InfoComponent.vue
+++ components/InfoComponent.vue
@@ -9,9 +9,18 @@
         </ol>
         <p class="font-semibold">Pie Chart</p>
         <ul>
-            <li>Outer circle = parent colors | <a href="https://sashamaps.net/docs/resources/20-colors/" target="_blank">source</a></li>
+            <li>Outer circle = parent colors | <a href="https://sashamaps.net/docs/resources/20-colors/" target="_blank" class="text-blue-600 underline">source</a></li>
             <li>Inner circle = actual colors</li>
         </ul>
+        <br />
+        <p class="font-semibold">How does it work?</p>
+        <ol class="ml-4 list-decimal">
+            <li>Converts the image into pixels</li>
+            <li>Sorts each pixel according to color</li>
+            <li>Finds clusters of pixels that are similiar colored</li>
+            <li>Counts the different colors and the amount of pixels per</li>
+            <li>Calculates the percantages of colors present in the image</li>
+        </ol>
         <br />
         <div class="pt-10 border-l-fuchsia-700">
             <p class="mb-2 text-xl">What's coming in version 1.5:</p>
