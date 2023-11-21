68c3dd2aa1497c08ef86def7a56168b2398ce0b7
HirenBudhAir <hirenbudhrani@gmail.com>
2023-11-21 03:33:18 +0100

    create separate component forr tooltip

---
+++ components/ColorPercentageTooltip.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="
+                    absolute 
+                    -top-48
+                    left-1/2 
+                    transform 
+                    -translate-x-1/2 
+                    bg-black 
+                    text-white 
+                    text-xs 
+                    px-2
+                    py-1 
+                    rounded 
+                    w-64
+                    flex 
+                    flex-col">
+        <div class="flex items-center space-x-2 font-bold">
+            <span>Actual color:</span>
+            <div class="w-3 h-3 border-2 border-white" :style="{ backgroundColor: color.html_code }"></div>
+        </div>
+        <div class="flex items-center space-x-2 font-bold">
+            <span>Closest Color::</span>
+            <div class="w-3 h-3 border-2 border-white" :style="{ backgroundColor: color.closest_palette_color_html_code }">
+            </div>
+        </div>
+        <div>Distance: {{ parseFloat(color.closest_palette_color_distance).toFixed(2) }}</div>
+        <div v-if="color.closest_palette_color_pantone">Pantone: {{ color.closest_palette_color_pantone }}</div>
+        <div>Name: {{ color.closest_palette_color }} Percent: {{ color.percent }}%</div>
+        <div class="font-bold">
+            <div class="flex items-center space-x-2 font-bold">
+                <span>Closest Parent Color:</span>
+                <div class="w-3 h-3 border-2 border-white"
+                    :style="{ backgroundColor: color.closest_palette_color_parent_html_code }" />
+            </div>
+        </div>
+        <div>Distance to closest color: {{ parseFloat(color.closest_palette_color_parent_distance).toFixed(2) }}</div>
+        <div>Name: {{ color.closest_palette_color_parent }}</div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        color: Object
+    }
+}
+</script>
--- components/ColorPercentages.vue
+++ components/ColorPercentages.vue
@@ -3,46 +3,13 @@
         Top colors
     </div>
     <div class="flex w-96 h-5 relative">
-        <div v-for="color in sortedColors" :style="`background-color: ${color.html_code}; width: ${color.percent}%`"
-            class="h-full transition-all duration-300 hover:bg-opacity-75 relative" @mouseover="hoverColor = color"
-            @mouseout="hoverColor = ''">
-            <div v-if="hoverColor === color"
-                class="
-                    absolute 
-                    -top-52
-                    left-1/2 
-                    transform 
-                    -translate-x-1/2 
-                    bg-black 
-                    text-white 
-                    text-xs 
-                    px-2
-                    py-1 
-                    rounded 
-                    w-64
-                    flex 
-                    flex-col"
-            >
-                <div class="flex items-center space-x-2 font-bold">
-                    <span>Actual color:</span>
-                    <div class="w-3 h-3 border-2 border-white" :style="{ backgroundColor: color.html_code }"></div>
-                </div>
-                <div class="flex items-center space-x-2 font-bold">
-                    <span>Closest Color::</span>
-                    <div class="w-3 h-3 border-2 border-white" :style="{ backgroundColor: color.closest_palette_color_html_code }"></div>
-                </div>
-                <div>Distance: {{ parseFloat(color.closest_palette_color_distance).toFixed(2) }}</div>
-                <div v-if="color.closest_palette_color_pantone">Pantone: {{ color.closest_palette_color_pantone }}</div>
-                <div>Name: {{ color.closest_palette_color }} Percent: {{ color.percent }}%</div>
-                <div class="font-bold">
-                    <div class="flex items-center space-x-2 font-bold">
-                        <span>Closest Parent Color:</span>
-                        <div class="w-3 h-3 border-2 border-white" :style="{ backgroundColor: color.closest_palette_color_parent_html_code }" />
-                    </div>
-                </div>
-                <div>Distance to closest color: {{ parseFloat(color.closest_palette_color_parent_distance).toFixed(2) }}</div>
-                <div>Name: {{ color.closest_palette_color_parent }}</div>
-            </div>
+        <div v-for="color in sortedColors" 
+            :style="`background-color: ${color.html_code}; width: ${color.percent}%`"
+            class="h-full transition-all duration-300 hover:bg-opacity-75 relative" 
+            @mouseover="hoverColor = color"
+            @mouseout="hoverColor = ''"
+        >
+            <ColorPercentageTooltip v-if="hoverColor === color" :color="color" />
         </div>
     </div>
 </template>
