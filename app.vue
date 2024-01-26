040cbe0c7d4ecb62b44c3e0b6fe4203ffa5226f3
HirenBudhAir <hirenbudhrani@gmail.com>
2024-01-26 18:24:12 +0100

    some neatifying

--- app.vue
+++ app.vue
@@ -15,8 +15,7 @@
 
       <!-- Info tooltip -->
       <div id="info-tooltip" class="ml-7" @click.prevent="showInfo = !showInfo">
-        <img v-if="!showInfo" class="w-9 cursor-pointer"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Minimalist_info_Icon.png/2048px-Minimalist_info_Icon.png" />
+        <img v-if="!showInfo" class="w-9 cursor-pointer" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Minimalist_info_Icon.png/2048px-Minimalist_info_Icon.png" />
         <img v-if="showInfo" class="w-9 cursor-pointer" src="https://www.svgrepo.com/show/24584/info-icon.svg" />
       </div>
       <InfoComponent v-if="showInfo" />
@@ -33,10 +32,8 @@
         <h3 class="text-lg font-semibold text-center mb-4">Save Preset</h3>
         <input v-model="newPresetName" placeholder="Enter Preset Name" class="w-full px-3 py-2 mb-3 border rounded" />
         <div class="flex justify-around">
-          <button @click="createPreset(newPresetName, presetPassword, processedImages)"
-            class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Save</button>
-          <button @click="showCreatePresetModal = false"
-            class="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">Cancel</button>
+          <button @click="createPreset(newPresetName, presetPassword, processedImages)" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Save</button>
+          <button @click="showCreatePresetModal = false" class="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">Cancel</button>
         </div>
       </div>
     </div>
@@ -47,9 +44,7 @@
         <path
           d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
           fill="currentColor" />
-        <path
-          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-          fill="currentFill" />
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
       </svg>
       <span class="sr-only">Loading...</span>
       <p>Processed {{ processedFiles }} out of {{ totalFiles }} files...</p>
@@ -321,8 +316,7 @@ export default {
           .catch((e) => {
             console.log('error', e)
           })
-        
-      } catch (error) { 
+      } catch (error) {
         console.error('Error creating preset:', error)
         alert('Failed to create the preset. Please try again.')
       }
