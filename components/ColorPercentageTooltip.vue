38fa0777dfc096f70da0d0dc8e98efd77da11f81
HirenBudhAir <hirenbudhrani@gmail.com>
2024-01-26 14:21:39 +0100

    tooltip bugfix

--- components/ColorPercentageTooltip.vue
+++ components/ColorPercentageTooltip.vue
@@ -19,7 +19,7 @@
             <div class="w-3 h-3 border-2 border-white" :style="{ backgroundColor: color.html_code }"></div>
             <div>| Percent: {{ parseFloat(color.percent).toFixed(2) }}%</div>
         </div>
-        <hr/>
+        <hr />
         <div class="flex items-center space-x-2 font-bold">
             <span>Closest Color:</span>
             <div class="w-3 h-3 border-2 border-white" :style="{ backgroundColor: color.closest_palette_color_html_code }">
@@ -27,17 +27,18 @@
         </div>
         <div>Distance: {{ parseFloat(color.closest_palette_color_distance).toFixed(2) }}</div>
         <div v-if="color.closest_palette_color_pantone">Pantone: {{ color.closest_palette_color_pantone }}</div>
-        <div>Name: {{ color.closest_palette_color }} 
-        <hr/>
-        <div class="font-bold">
-            <div class="flex items-center space-x-2 font-bold">
-                <span>Closest Parent Color:</span>
-                <div class="w-3 h-3 border-2 border-white"
-                    :style="{ backgroundColor: color.closest_palette_color_parent_html_code }" />
+        <div>Name: {{ color.closest_palette_color }}
+            <hr />
+            <div class="font-bold">
+                <div class="flex items-center space-x-2 font-bold">
+                    <span>Closest Parent Color:</span>
+                    <div class="w-3 h-3 border-2 border-white"
+                        :style="{ backgroundColor: color.closest_palette_color_parent_html_code }" />
+                </div>
             </div>
+            <div>Distance to closest color: {{ parseFloat(color.closest_palette_color_parent_distance).toFixed(2) }}</div>
+            <div>Name: {{ color.closest_palette_color_parent }}</div>
         </div>
-        <div>Distance to closest color: {{ parseFloat(color.closest_palette_color_parent_distance).toFixed(2) }}</div>
-        <div>Name: {{ color.closest_palette_color_parent }}</div>
     </div>
 </template>
 <script>
